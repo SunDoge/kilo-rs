904f66962c10ef39f0dd4ef6b7e110a3bb019e97
SunDoge <384813529@qq.com>
2018-10-21 11:31:33 +0800

    welcome string

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::env;
-use std::io::{self, Stdin, Stdout, Write};
+use std::fs::File;
+use std::io::{self, BufRead, Stdin, Stdout, Write};
 use termion::event::Key;
 use termion::input::{Keys, TermRead};
 use termion::raw::{IntoRawMode, RawTerminal};
-use termion::terminal_size;
+use termion::{clear, cursor, terminal_size};
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
@@ -12,7 +13,7 @@ struct Config {
     cy: u16,
     screencols: u16,
     screenrows: u16,
-    numrows: u16,
+    rows: Vec<String>,
 }
 
 impl Config {
@@ -24,7 +25,7 @@ impl Config {
             cy: 0,
             screencols: w,
             screenrows: h,
-            numrows: 0,
+            rows: Vec::new(),
         }
     }
 }
@@ -39,15 +40,9 @@ struct Editor {
 impl Editor {
     pub fn new() -> Editor {
         let stdin = io::stdin();
-        let mut stdout = io::stdout()
+        let stdout = io::stdout()
             .into_raw_mode()
             .expect("unable to enable raw mode");
-
-        let args: Vec<String> = env::args().collect();
-
-        if args.len() > 2 {
-            // self.open(&args[2]);
-        }
 
         Editor {
             stdin: stdin.keys(),
@@ -57,18 +52,41 @@ impl Editor {
         }
     }
 
-    pub fn open(&mut self, filename: &str) {}
+    pub fn open(&mut self, filename: &str) -> io::Result<()> {
+        let f = File::open(filename)?;
 
-    pub fn refresh_screen(&self) {}
+        for line in io::BufReader::new(f).lines() {
+            self.config.rows.push(line?);
+        }
+
+        Ok(())
+    }
+
+    pub fn refresh_screen(&mut self) {
+        self.buffer
+            .push_str(&format!("{}{}", cursor::Hide, cursor::Goto::default()));
+        self.draw_rows();
+        self.buffer.push_str(&format!(
+            "{}{}",
+            cursor::Goto(self.config.cy + 1, self.config.cx + 1),
+            cursor::Show
+        ));
+        write!(self.stdout, "{}", self.buffer);
+        self.buffer.clear();
+    }
 
     pub fn process_key_press(&mut self) -> bool {
-        let c = self.read_key();
+        let c = self.read_key().unwrap();
 
-        match c.unwrap() {
+        match c {
             Key::Ctrl(k) => match k {
-                'q' => return false,
+                'q' => {
+                    write!(self.stdout, "{}{}", clear::All, cursor::Goto::default());
+                    return false;
+                }
                 _ => {}
             },
+            Key::Up | Key::Down | Key::Left | Key::Right => self.move_cursor(c),
             _ => {}
         }
 
@@ -78,11 +96,9 @@ impl Editor {
     pub fn set_status_message(&mut self, msg: &str) {}
 
     pub fn run(&mut self) {
-        loop {
+        self.refresh_screen();
+        while self.process_key_press() {
             self.refresh_screen();
-            if self.process_key_press() == false {
-                break;
-            }
         }
     }
 }
@@ -94,13 +110,13 @@ impl Editor {
 
     fn draw_rows(&mut self) {
         for y in 0..self.config.screenrows {
-            if y > self.config.numrows {
-                if self.config.numrows == 0 && y == self.config.screenrows / 3 {
+            if y as usize > self.config.rows.len() {
+                if self.config.rows.len() == 0 && y == self.config.screenrows / 3 {
                     let welcome = format!("Kilo editor -- version {}", VERSION);
 
                     let mut welcomelen = welcome.len() as u16;
 
-                    if (welcomelen > self.config.screencols) {
+                    if welcomelen > self.config.screencols {
                         welcomelen = self.config.screencols;
                     }
 
@@ -119,12 +135,49 @@ impl Editor {
             } else {
 
             }
+
+            self.buffer.push_str(&format!("{}", clear::UntilNewline));
+            if y < self.config.screenrows - 1 {
+                self.buffer.push_str("\r\n");
+            }
+        }
+    }
+
+    fn move_cursor(&mut self, key: Key) {
+        match key {
+            Key::Left => {
+                if self.config.cx != 0 {
+                    self.config.cx -= 1;
+                }
+            }
+            Key::Right => {
+                if self.config.cx != self.config.screencols - 1 {
+                    self.config.cx += 1;
+                }
+            }
+            Key::Up => {
+                if self.config.cy != 0 {
+                    self.config.cy -= 1;
+                }
+            }
+            Key::Down => {
+                if self.config.cy != self.config.screenrows {
+                    self.config.cy += 1;
+                }
+            }
+            _ => {}
         }
     }
 }
 
 fn main() {
     let mut editor = Editor::new();
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        editor.open(&args[2]).expect("fail to open");
+    }
 
     editor.set_status_message("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
